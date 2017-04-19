a528e5e7b82536a4a9a81e72959fc02514c392c6
David Hatri <david@brightmountainmedia.com>
2017-04-19 11:34:51 -0400

    Add middleware group that allows trottle to 1k per minute

--- routes/api.php
+++ routes/api.php
@@ -14,20 +14,18 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group(['middleware' => 'throttle:1000,1'], function() {
         Route::get('/widget_title', function (Request $request) {
             $widget_title = Settings::where('name', 'widget_title')->first();
             return $widget_title->value;
-        });
+        })->middleware('throttle:1000,1');
 
         Route::get('/widget_count', function (Request $request) {
             $widget_count = Settings::where('name', 'widget_count')->first();
             return $widget_count->value;
-        });
+        })->middleware('throttle:1000,1');
 
         Route::get('/widget_articles', function (Request $request) {
             $widget_count = Settings::where('name', 'widget_count')->first();
             $articles = Article::widget()->active()->orderBy('order', 'asc')->limit($widget_count->value)->get();
             return $articles;
-        });
-});
+        })->middleware('throttle:1000,1');
